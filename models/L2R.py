768f2e20c86533fab4b8bd67d29b5f4823ea1cad
HioDza <randompangeran@gmail.com>
2025-12-27 18:55:16 +0700

    L2R

--- models/L2R.py
+++ models/L2R.py
@@ -113,7 +113,7 @@ class L2Regressor:
             
         ## Raises:
             **ValueError**: *If input data contains NaN/Inf or if dimensions mismatch.*
-            **np.linalg.LinAlgError**: *If $X^T X + \lambda I$ is singular (rare in Ridge Regression).*
+            **np.linalg.LinAlgError**: *If X^T X + lambda I is singular (rare in Ridge Regression).*
         """
         # Data validation and conversion
         if issparse(X_train):
